243aea3fd65348ae33bd2b1a5636f45e52dfefef
Rick Smoke <rsmoke@umich.edu>
2022-05-18 17:21:28 -0400

    add clearing of input fields

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,12 +1,11 @@
 class Room < ApplicationRecord
   has_many :participants, dependent: :destroy
+  has_many :messages
+
   after_create_commit { broadcast_if_public }
 
   validates_uniqueness_of :name
   scope :public_rooms, -> { where(is_private: false) }
-  after_create_commit {broadcast_append_to "rooms"}
-
-  has_many :messages
 
   def broadcast_if_public
     broadcast_append_to "rooms" unless self.is_private
