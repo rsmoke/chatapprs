54d8baa658da16b3635bc35d7b707e172019ae4b
Rick Smoke <rsmoke@umich.edu>
2022-05-18 16:40:46 -0400

    add messaging

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :rooms
+  resources :rooms do
+    resources :messages
+  end
   resources :users
   root 'rooms#index'
   
