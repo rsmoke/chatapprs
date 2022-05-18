808fd8b2af3606b1aa225f97e78e7f76b0fdc6fb
Rick Smoke <rsmoke@umich.edu>
2022-05-18 16:07:55 -0400

    create views add initial records

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
-  get 'sessions/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  resources :rooms
+  resources :users
+  root 'rooms#index'
+  
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
